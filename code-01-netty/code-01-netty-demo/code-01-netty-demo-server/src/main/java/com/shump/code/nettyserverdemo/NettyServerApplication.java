7328f06f59d71df602e41f61042d5d82453935ef
Zhou <2792910237@qq.com>
2023-09-13 10:44:14 +0800

    netty 服务端初步构建

---
+++ code-01-netty/code-01-netty-demo/code-01-netty-demo-server/src/main/java/com/shump/code/nettyserverdemo/NettyServerApplication.java
@@ -0,0 +1,17 @@
+package com.shump.code.nettyserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zcq
+ * @Classname
+ * @date 2023/9/8
+ * @Description
+ */
+@SpringBootApplication
+public class NettyServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApplication.class, args);
+    }
+}
